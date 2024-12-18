d2269359e61982a27d08f5ec50e813ffd99bba81
Huy Ong <ogh.huy@gmail.com>
2024-12-18 11:27:55 -0800

    Add CD

--- json.go
+++ json.go
@@ -27,8 +27,8 @@ func respondWithJSON(w http.ResponseWriter, code int, payload interface{}) {
 		return
 	}
 	w.WriteHeader(code)
-	if _, err := w.Write(dat); err != nil {
-		log.Printf("Error writing response: %s", err)
-		return
+	_, err = w.Write(dat)
+	if err != nil {
+		log.Printf("Critical error writing response: %s", err)
 	}
 }
